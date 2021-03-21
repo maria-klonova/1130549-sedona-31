d83b131b5070c3d366d7972d322081b12d43a193
m-klonova <maria.klonova@gmail.com>
2021-03-21 16:41:33 +0300

    проект

--- js/script.js
+++ js/script.js
@@ -1,6 +1,8 @@
 const searchButton = document.getElementById("search-form__button");
 const searchForm = document.getElementById("search-form");
 
-searchButton.addEventListener("click", () => {
-  searchForm.classList.toggle("search-form--active");
-});
+if (searchButton && searchForm) {
+  searchButton.addEventListener("click", () => {
+    searchForm.classList.toggle("search-form--active");
+  });
+}
